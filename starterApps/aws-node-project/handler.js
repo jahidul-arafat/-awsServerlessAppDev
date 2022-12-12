fd00ed10575ca2cfc6fbd4ccae33e73aebdc6742
jahidul-arafat <jahidapon@gmail.com>
2022-12-12 23:04:19 +0600

    serverless updates

--- starterApps/aws-node-project/handler.js
+++ starterApps/aws-node-project/handler.js
@@ -1,12 +1,15 @@
 'use strict';
 
-module.exports.hello = async (event) => {
+module.exports.hello = async (event,context) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
-        input: event,
+        messageCore: 'Go Serverless v3.0! Your function executed successfully!',
+        myMessage: 'Welcome to jarafatkata.com site launching ceremony!',
+        secondMsg: 'Have Patience, Have faith on ALLAH (SWT)',
+        eventData: event,
+        contextData: context
       },
       null,
       2
